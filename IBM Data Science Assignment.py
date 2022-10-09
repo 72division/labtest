123ee73e5bab282ba954e2568125f3adf395575e
72division <46007544+72division@users.noreply.github.com>
2022-10-09 17:30:28 +0900

    Add files via upload

---
+++ IBM Data Science Assignment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook in IBM Watson Studio
+
+# **SungGone Kim**
+# <br>current occupation : Product Manager
+
+# *I am interested in data sciecne because I want to build a machine learning model model for sound classification. I want to build a application analyzing sleep environment by classifying noises and snoring sound.*
+
+# ### below code will do basic mathmatics
+
+# In[2]:
+
+
+(15+3)/3
+
+
+# ***
+# | my skills |
+# |:---|
+#  1. Matlab 
+#  2. __[Notion](https://notion.so)__
+#  3. Swift
+#  
+
+# In[ ]:
+
+
+
+
